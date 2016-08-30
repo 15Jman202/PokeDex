03d0b2658ed93d34b19f2759a1410e37750178d1
Justin Carver <15Jcarver202@gmail.com>
2016-08-30 12:00:42 -0600

    Finished and working

--- PokeDex/PokeDex/PokemonSearchViewController.swift
+++ PokeDex/PokeDex/PokemonSearchViewController.swift
@@ -8,36 +8,39 @@
 
 import UIKit
 
-class PokemonSearchViewController: UIViewController {
+class PokemonSearchViewController: UIViewController, UISearchBarDelegate {
+    
+    
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var nameLable: UILabel!
     @IBOutlet weak var baseStatLable: UILabel!
     @IBOutlet weak var moveLable: UILabel!
     @IBOutlet weak var formImage: UIImageView!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.searchBar.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else { return }
+        PokemonController.getPokemon(searchTerm) { (pokemon) in
+            guard let pokemon = pokemon else { return }
+            
+            self.nameLable.text = pokemon.name.capitalizedString
+            self.baseStatLable.text = "\(pokemon.baseStat)"
+            self.moveLable.text = pokemon.moves.capitalizedString
+            
+            PokemonController.getSpriteEndpointForPokemon(pokemon, completion: { (endpoint) in
+                guard let newendpoint = endpoint else { return }
+                
+                ImageController.imageForURL(newendpoint, completion: { (image) in
+                    guard let image = image else { return }
+                    
+                    self.formImage.image = image
+                })
+            })
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
